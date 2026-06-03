c7e1a282b0d038d053165f2b838ab073d16a8b63
Jackeline de Paula <jackeline.delima17@gmail.com>
2026-06-03 19:11:16 -0300

    adicionando model do mapa
    A model cuida de quais campos há nas tabelas que registram os pontos turistícos.

---
+++ AtividadeFinal/guiaturisticosr/lib/model/ponto_turistico_model.dart
@@ -0,0 +1,18 @@
+//campos do mapa
+class PontosTuristicos {
+  final String nome;
+  final String categoria;
+  final double latitude;
+  final double longitude;
+  final String descricao;
+  final bool rotaDoVinho;
+
+  PontosTuristicos({
+    required this.nome,
+    required this.categoria,
+    required this.latitude,
+    required this.longitude,
+    required this.descricao,
+    required this.rotaDoVinho,
+  });
+}
