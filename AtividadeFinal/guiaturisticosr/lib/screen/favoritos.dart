d90e1e536f2c8d4b6528acf5449ffa9eaf1d638f
Jackeline de Paula <jackeline.delima17@gmail.com>
2026-06-04 14:48:28 -0300

    Adicionando pagina de favoritos
    Adicionando pagina de favoritos sem codigo até o momento, onte tem como objetivo o usuario ver quais restaurantes/hoteis e pontos turisticos favoritou para ver depois.

---
+++ AtividadeFinal/guiaturisticosr/lib/screen/favoritos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class FavoritosScreen extends StatefulWidget {
+  const FavoritosScreen({super.key});
+
+  @override
+  State<FavoritosScreen> createState() => _FavoritosScreenState();
+}
+
+class _FavoritosScreenState extends State<FavoritosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
