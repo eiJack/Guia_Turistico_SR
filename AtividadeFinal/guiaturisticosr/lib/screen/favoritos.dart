98e56b76daa6662d070d8271c1a3e594ac16ddaf
crystybarros <crystybarros@hotmail.com>
2026-06-07 19:09:19 -0300

    Ajuste na tela de favoritos, criando link para o detalhes do ponto turistico.

--- AtividadeFinal/guiaturisticosr/lib/screen/favoritos.dart
+++ AtividadeFinal/guiaturisticosr/lib/screen/favoritos.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../service/favoritos_service.dart';
+import 'package:guiaturisticosr/data/pontos_turisticos.dart';
+import 'package:guiaturisticosr/screen/detalhes_ponto.dart';
 
 class FavoritosScreen extends StatelessWidget {
   const FavoritosScreen({super.key});
@@ -33,6 +35,28 @@ class FavoritosScreen extends StatelessWidget {
                   leading: Image.asset(dados['imagem'], width: 60, fit: BoxFit.cover),
                   title: Text(dados['nome']),
                   subtitle: Text(dados['descricao'], maxLines: 2, overflow: TextOverflow.ellipsis),
+
+                  trailing: const Icon(Icons.arrow_forward_ios),
+
+                  onTap: () {
+                    final todosOsPontos = [...rotaDoVinho, ...hoteis, ...turismo];
+
+                    final ponto = todosOsPontos.firstWhere((p) => p['nome'] == dados['nome']);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => DetalhesPonto(
+                          nome: ponto['nome'] ?? '',
+                          imagem: ponto['imagem'] ?? '',
+                          descricao: ponto['descricao'] ?? '',
+                          telefone: ponto['telefone'] ?? '',
+                          whatsapp: ponto['whatsapp'] ?? '',
+                          endereco: ponto['endereco'] ?? '',
+                        ),
+                      ),
+                    );
+                  },
                 ),
               );
             },
